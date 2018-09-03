843c48a40ce4e07dae24d35ec723f1d0562926c4
Mohamed Ebrahim <ebrahimm131@gmail.com>
2018-09-03 10:20:16 +0200

    fix all bugs

--- app/src/main/java/com/vacuum/fragmentnavigationmvp/ui/main/MainActivity.kt
+++ app/src/main/java/com/vacuum/fragmentnavigationmvp/ui/main/MainActivity.kt
@@ -6,26 +6,28 @@ import com.vacuum.fragmentnavigationmvp.ui.base.BaseFragment
 import com.vacuum.fragmentnavigationmvp.ui.main.MainMvpView
 import com.vacuum.fragmentnavigationmvp.ui.main.MainPresenter
 
+
 class MainActivity : AppCompatActivity(), MainMvpView {
 
+    private var presenter: MainPresenter? = null
 
-    var presenter: MainPresenter? = null
     //https://medium.com/@urmanschi.mihail/mvp-fragment-navigation-from-inside-fragments-2c4388f97904
     //https://github.com/dsdmsa/FragmentNavigationMVP
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        presenter = MainPresenter(this)
 
-        presenter?.getRandomFragment()
+        presenter!!.getRandomFragment()
     }
 
-    override fun setFragment(basefragmnt: BaseFragment) {
-
-        basefragmnt.atachPresenter(presenter!!)
+    override fun setFragment(fragment: BaseFragment) {
+        //ataching to fragment the navigation presenter
+        fragment.atachPresenter(presenter!!)
         //showing the presenter on screen
-        getSupportFragmentManager()
+        supportFragmentManager
                 .beginTransaction()
-                .replace(R.id.fragment_container,basefragmnt)
+                .replace(R.id.fragment_container, fragment)
                 .commit()
     }
 }
